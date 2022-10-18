69e1dd2f5fc36e2e3cadd8b56f5b41f0f3771350
Paul Norling <paulnorling87@gmail.com>
2022-10-18 12:03:52 -0500

    Added a function named addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,7 +10,15 @@ let collection = [];
 //   - Create a new object having the above properties
 //   - Add the new object to the end of the `collection` array
 //   - Return the newly created object
-
+function addToCollection(title, artist, yearPublished){
+  let album = {
+    Title: title,
+    Artist: artist,
+    Year: yearPublished,
+  }
+  collection.push(album);
+  return album;
+  }
 // - Test the `addToCollection` function:
 //   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
 //   - Console.log each album as added using the returned value.
