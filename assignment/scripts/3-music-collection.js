3310a3d20207264de628bbc068bbd8398711745e
Paul Norling <paulnorling87@gmail.com>
2022-10-18 14:17:58 -0500

    added showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -23,7 +23,7 @@ function addToCollection(title, artist, yearPublished){
 //   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
 //   - Console.log each album as added using the returned value.
 //   - After all are added, console.log the `collection` array.
-addToCollection("nevermind", "Nirvana", "1991");
+addToCollection("Nevermind", "Nirvana", "1991");
 addToCollection('In Utero', 'Nirvana', '1993');
 addToCollection('Third Eye Blind', 'Third Eye Blind', '1997');
 addToCollection('So Much for the Afterglow', 'Everclear', '1997');
@@ -34,9 +34,14 @@ console.log(collection);
 //   - Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
 //   - Console.log the number of items in the array.
 //   - Loop over the array and console.log each album's information formatted like: `TITLE by ARTIST, published in YEAR`.
-
+function showCollection(array){
+  console.log(array.length);
+  for (let album of array){
+    console.log(album.Title, 'by', album.Artist, 'published in', album.Year)
+    }
+}
 // - Test the `showCollection` function.
-
+showCollection(collection);
 // - Add a function named `findByArtist`. This function should:
 //   - Take in `artist` (a string) parameter
 //   - Create an array to hold any results, empty to start
