ce970eb6954d53626cb76c521e0ca812b26ce229
Paul Norling <paulnorling87@gmail.com>
2022-10-18 12:24:43 -0500

    added albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -23,7 +23,13 @@ function addToCollection(title, artist, yearPublished){
 //   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
 //   - Console.log each album as added using the returned value.
 //   - After all are added, console.log the `collection` array.
-
+addToCollection("nevermind", "Nirvana", "1991");
+addToCollection('In Utero', 'Nirvana', '1993');
+addToCollection('Third Eye Blind', 'Third Eye Blind', '1997');
+addToCollection('So Much for the Afterglow', 'Everclear', '1997');
+addToCollection('Rumors', 'Fleetwoood Mac', '1977');
+addToCollection('Dive','Tycho','2011');
+console.log(collection);
 // - Add a function named `showCollection`. This function should:
 //   - Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
 //   - Console.log the number of items in the array.
